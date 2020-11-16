5c1bdc3c15431b3e5536d0b5ed283595b667d281
Benji <50681275+Benji377@users.noreply.github.com>
2020-11-16 14:48:17 +0100

    Update main.py
    Added the first function to the project

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 print("OpenCode")
 
+# Python program to print ASCII Value of Character
+def ascii_value():
+    character_input = input("Enter Character you want to know the ASCII Value of: ")
+    try:
+        ascii_number = ord(character_input)
+        print("The ASCII value of '" + character_input + "' is", ascii_number)
+    except TypeError:
+        print("Please input a single character")
+        return ascii_value()
+
+
+ascii_value()
