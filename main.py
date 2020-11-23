290961a0c8a4e2679761d6c04c55ef0e0b23d03c
Benji <50681275+Benji377@users.noreply.github.com>
2020-11-23 19:37:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from pyfiglet import Figlet
 
+
 def header():
     """Show the name as ASCII art."""
     f = Figlet(font='slant')
     f = f.renderText("OpenCode")
     print(f)
+
+
+# Gives you the ability to select how to use the program
+def selection():
+    active = True
+    print('Welcome to Opencode! \n'
+          'To get started, type the number of the list that refers to the thing you want to do\n'
+          'To get the full list, just type "0"')
+
+    while active:
+        list_number = int(input("Enter the number here: "))
+        if list_number == 0:
+            print("1 - Ascii calculator: Gives you the Ascii value of a character")
+            continue
+        elif list_number == 1:
+            active = False
+            ascii_value()
+        else:
+            print("Not a valid number")
+            continue
+
 
 # Python program to print ASCII Value of Character
 def ascii_value():
@@ -16,5 +38,6 @@ def ascii_value():
         print("Please input a single character")
         return ascii_value()
 
+
 header()
-ascii_value()
+selection()
